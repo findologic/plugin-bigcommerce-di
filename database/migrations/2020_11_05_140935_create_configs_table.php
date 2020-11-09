32a0ebec1064251d0f29d80552d6405e1925687d
Daniel Blaichinger <d.blaichinger@findologic.com>
2020-11-09 08:55:48 +0100

    Add migrations for stores and configs table

---
+++ database/migrations/2020_11_05_140935_create_configs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configs', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->string('shopkey');
+            $table->string('language');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configs');
+    }
+}
