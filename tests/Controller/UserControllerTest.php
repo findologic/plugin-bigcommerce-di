2ebc63a9aee53f5a1d3115a7ce0fb526d7a3d1dc
Daniel Blaichinger <d.blaichinger@findologic.com>
2021-04-26 12:31:50 +0200

    Fixes UsercontrollerTest remove callback

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -7,11 +7,14 @@ use App\Models\Store;
 use App\Models\User;
 use App\Services\BigCommerceService;
 use Illuminate\Http\Request;
-use PHPUnit\Framework\TestCase;
+use TestCase;
 
 class UserControllerTest extends TestCase
 {
     private BigCommerceService $bigCommerceServiceMock;
+    private User $owner;
+    private User $user;
+    private Store $store;
 
     public function setUp(): void
     {
@@ -21,37 +24,46 @@ class UserControllerTest extends TestCase
             ->onlyMethods(['verifySignedRequest'])
             ->disableOriginalConstructor()
             ->getMock();
+
+        $this->store = new Store([
+            'context' => 'stores/test123',
+            'access_token' => '1234',
+        ]);
+        $this->store->save();
+
+        $this->owner = new User([
+            'username' => 'Store Owner',
+            'email' => 'owner@store.com',
+            'role' => 'owner',
+            'bigcommerce_user_id' => 1,
+            'store_id' => $this->store->id
+        ]);
+        $this->owner->save();
+
+        $this->user = new User([
+            'username' => 'Store user',
+            'email' => 'john@doe.com',
+            'role' => 'user',
+            'bigcommerce_user_id' => 2,
+            'store_id' => $this->store->id
+        ]);
+        $this->user->save();
     }
 
     public function testStoreIsSavedWhenInstallationIsSuccessful()
     {
-        $store = new Store([
-            'context' => 'stores/test123',
-            'access_token' => '1234'
-        ]);
-        $store->save();
-
-        $user = new User([
-            'username' => 'John Doe',
-            'email' => 'john@doe.com',
-            'role' => 'user',
-            'bigcommerce_user_id' => 125689,
-            'store_id' => $store->id
-        ]);
-        $user->save();
-
         $this->bigCommerceServiceMock->method('verifySignedRequest')->willReturn([
             'user' => [
-                'id' => 125689,
-                'username' => 'John Doe',
-                'email' => 'john@doe.com'
+                'id' => $this->user->bigcommerce_user_id,
+                'username' => $this->user->username,
+                'email' => $this->user->email,
             ],
             'owner' => [
-                'id' => 125689,
-                'username' => 'John Doe',
-                'email' => 'john@doe.com'
+                'id' => $this->owner->bigcommerce_user_id,
+                'username' => $this->owner->username,
+                'email' => $this->owner->email
             ],
-            'context' => 'stores/test123',
+            'context' => $this->store->context,
             'store_hash' => 'test123',
             'timestamp' => time(),
         ]);
@@ -60,7 +72,10 @@ class UserControllerTest extends TestCase
         $request = Request::create('/user/remove', 'GET', ['signed_payload' => '1234']);
         $userController->remove($request, $this->bigCommerceServiceMock);
 
-        $user = User::where('id', $user->id)->first();
-        $this->assertCount(0, $user);
+        // User from remove callback should be gone and owner still exists
+        $user = User::where('id', $this->user->id)->first();
+        $this->assertNull($user);
+        $owner = User::where('id', $this->owner->id)->first();
+        $this->assertInstanceOf(User::class, $owner);
     }
 }
